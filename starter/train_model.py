d8cecebc2aa4ce05eafc8b6792a49b1565820147
marcospiau <marcospiau123@gmail.com>
2022-04-04 01:48:15 -0300

    Start changes for model training

--- starter/train_model.py
+++ starter/train_model.py
@@ -2,29 +2,38 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from ml.data import process_data
 
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
-data = pd.read_csv('../data/census-fix-spaces-nodups.csv')
+data = pd.read_csv('../data/processed/census-fix-spaces-nodups.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(data, test_size=0.20, stratify=data['salary'])
 
 cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
+    'workclass',
+    'education',
+    'marital-status',
+    'occupation',
+    'relationship',
+    'race',
+    'sex',
+    'native-country',
 ]
 X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
+    train, categorical_features=cat_features, label='salary', training=True
 )
-
 # Proces the test data with the process_data function.
+
+X_test, y_test, _, _ = process_data(
+    X=data,
+    categorical_features=cat_features,
+    label='salary',
+    training=False,
+    encoder=encoder,
+    lb=lb
+)
 
 # Train and save a model.
