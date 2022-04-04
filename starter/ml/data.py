b752fd12c5fdfbd5b4672d25f12e33c2e2300d1d
marcospiau <marcospiau123@gmail.com>
2022-04-04 03:03:13 -0300

    - add processing and training pipeline on dvc   - first working version, simple DecisionTreeClassifier   - flake8 lint runs without error   - save model to dvc   - save train and test data to dvc   - track metrics using dvc   - save categorical encoder and label binarizer on dvc

--- starter/ml/data.py
+++ starter/ml/data.py
@@ -2,9 +2,12 @@ import numpy as np
 from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
 
 
-def process_data(
-    X, categorical_features=[], label=None, training=True, encoder=None, lb=None
-):
+def process_data(X,
+                 categorical_features=[],
+                 label=None,
+                 training=True,
+                 encoder=None,
+                 lb=None):
     """ Process the data used in the machine learning pipeline.
 
     Processes the data using one hot encoding for the categorical features and a
@@ -54,7 +57,7 @@ w
     X_continuous = X.drop(*[categorical_features], axis=1)
 
     if training is True:
-        encoder = OneHotEncoder(sparse=False, handle_unknown="ignore")
+        encoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
         lb = LabelBinarizer()
         X_categorical = encoder.fit_transform(X_categorical)
         y = lb.fit_transform(y.values).ravel()
