5380c7d1aac20b8261625a34baf75ef72cc741a3
Marcos Piau Vieira <marcospiau123@gmail.com>
2022-04-07 00:00:20 -0300

    Implement and integrate slice metrics calculations
    - change docstring to Google style
    - add metrics slice function
    - add function to pretty print pandas dataframes

--- starter/ml/data.py
+++ starter/ml/data.py
@@ -1,52 +1,51 @@
+from typing import Tuple
+
 import numpy as np
+import pandas as pd
 from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
 
 
-def process_data(X,
-                 categorical_features=[],
-                 label=None,
-                 training=True,
-                 encoder=None,
-                 lb=None):
-    """ Process the data used in the machine learning pipeline.
+def process_data(
+        X: pd.DataFrame,
+        categorical_features=[],
+        label=None,
+        training=True,
+        encoder=None,
+        lb=None) -> Tuple[np.array, np.array, OneHotEncoder, LabelBinarizer]:
+    """Process the data used in the machine learning pipeline.
 
-    Processes the data using one hot encoding for the categorical features and a
-    label binarizer for the labels. This can be used in either training or
-    inference/validation.
+        Processes the data using one hot encoding for the categorical features
+        and a label binarizer for the labels. This can be used in either
+        training or inference/validation.
 
-    Note: depending on the type of model used, you may want to add in functionality that
-    scales the continuous data.
+        Note: depending on the type of model used, you may want to add in
+        functionality that scales the continuous data.
 
-    Inputs
-    ------
-    X : pd.DataFrame
-        Dataframe containing the features and label. Columns in `categorical_features`
-    categorical_features: list[str]
-        List containing the names of the categorical features (default=[])
-    label : str
-        Name of the label column in `X`. If None, then an empty array will be returned
-        for y (default=None)
-    training : bool
-        Indicator if training mode or inference/validation mode.
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
-        Trained sklearn OneHotEncoder, only used if training=False.
-    lb : sklearn.preprocessing._label.LabelBinarizer
-        Trained sklearn LabelBinarizer, only used if training=False.
+    Args:
+        X (pd.DataFrame): Dataframe containing the features and label.
+            Columns in `categorical_features`.
+        categorical_features (list, optional): List containing the names of
+            the categorical features (default=[]). Defaults to [].
+        label (str, optional): Name of the label column in `X`. If None, then
+            an empty array will be returned for y (default=None).
+            Defaults to None.
+        training (bool, optional): Indicator if training mode or
+            inference/validation mode.. Defaults to True.
+        encoder (OneHotEncoder, optional): Trained sklearn OneHotEncoder, only
+            used if training=False. Defaults to None.
+        lb (LabelBinarizer, optional): Trained sklearn LabelBinarizer, only
+            used if training=False. Defaults to None.
 
-    Returns
-    -------
-    X : np.array
-        Processed data.
-    y : np.array
-        Processed labels if labeled=True, otherwise empty np.array.
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
-        Trained OneHotEncoder if training is True, otherwise returns the encoder passed
-        in.
-    lb : sklearn.preprocessing._label.LabelBinarizer
-        Trained LabelBinarizer if training is True, otherwise returns the binarizer
-        passed in.
+    Returns:
+        Tuple[, np.array, OneHotEncoder, LabelBinarizer]:
+            X (np.array): Processed data.
+            y (np.array): Processed labels if labeled=True, otherwise
+                empty np.array.
+            encoder (OneHotEncoder): Trained OneHotEncoder if training is True,
+                otherwise returns the encoder passed in.
+            lb (LabelBinarizer): Trained LabelBinarizer if training is True,
+                otherwise returns the binarizer passed in.
     """
-
     if label is not None:
         y = X[label]
         X = X.drop([label], axis=1)
--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,6 +1,9 @@
 from typing import Dict
 
+import more_itertools
 import numpy as np
+import pandas as pd
+import tabulate
 from sklearn.base import BaseEstimator
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.tree import DecisionTreeClassifier
@@ -8,19 +11,14 @@ from sklearn.tree import DecisionTreeClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train: np.array, y_train: np.array) -> BaseEstimator:
-    """
-    Trains a machine learning model and returns it.
+    """Trains a machine learning model and returns it.
 
-    Inputs
-    ------
-    X_train : np.array
-        Training data.
-    y_train : np.array
-        Labels.
-    Returns
-    -------
-    model
-        Trained machine learning model.
+    Args:
+        X_train (np.array): Training data.
+        y_train (np.array): Labels.
+
+    Returns:
+        BaseEstimator: Trained machine learning model.
     """
     model = DecisionTreeClassifier(max_depth=6)
     model.fit(X_train, y_train)
@@ -28,20 +26,16 @@ def train_model(X_train: np.array, y_train: np.array) -> BaseEstimator:
 
 
 def compute_model_metrics(y: np.array, preds: np.array) -> Dict[str, float]:
-    """
-    Validates the trained machine learning model using precision, recall, and F1.
+    """Validates the trained machine learning model using precision, recall,
+        and f1.
 
-    Inputs
-    ------
-    y : np.array
-        Known labels, binarized.
-    preds : np.array
-        Predicted labels, binarized.
-    Returns
-    -------
-    precision : float
-    recall : float
-    fbeta : float
+    Args:
+        y (np.array): Known labels, binarized.
+        preds (np.array): Predicted labels, binarized.
+
+    Returns:
+        Dict[str, float]: Dict mapping metrics ('precision', 'recall', 'f1')
+        to float values.
     """
     fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
     precision = precision_score(y, preds, zero_division=1)
@@ -49,21 +43,72 @@ def compute_model_metrics(y: np.array, preds: np.array) -> Dict[str, float]:
     return {'f1': fbeta, 'precision': precision, 'recall': recall}
 
 
-# TODO: configure threshold
-# TODO: verify if data must be preprocessed prior to this point
-# TODO: fill docstring
 def inference(model: BaseEstimator, X: np.array) -> np.array:
-    """ Run model inferences and return the predictions.
+    """Run model inferences and return the predictions.
 
-    Inputs
-    ------
-    model : BaseEstimator
-        Trained machine learning model.
-    X : np.array
-        Data used for prediction.
-    Returns
-    -------
-    preds : np.array
-        Predictions from the model.
+    Args:
+        model (BaseEstimator): Trained machine learning model.
+        X (np.array): Trained machine learning model.
+
+    Returns:
+        np.array: Predictions from the model.
     """
     return model.predict(X)
+
+
+# TODO: add tests for this function
+def compute_metrics_by_groups(y: np.array, preds: np.array,
+                              groups: np.array) -> pd.DataFrame:
+    """Compute metrics by each group in groups.
+
+    Args:
+        y (np.array): Known labels, binarized.
+        preds (np.array): Predicted labels, binarized.
+        groups (np.array): Groups to be analyzed
+
+    Returns:
+        pd.DataFrame, where index are group values, columns are metric values.
+        sMetrics are f1, precision, recall and support
+    """
+
+    def reduce_calc_metrics(idxs):
+        idxs = np.array(idxs)
+        out = compute_model_metrics(y=y[idxs], preds=preds[idxs])
+        out.update({'support': len(idxs)})
+        return out
+
+    # get dict mapping grouup_value to index
+    out = more_itertools.map_reduce(
+        iterable=enumerate(groups),
+        keyfunc=lambda x: x[1],  # group value
+        valuefunc=lambda x: x[0],  # idx value
+        reducefunc=reduce_calc_metrics  # convert to np.array
+    )
+
+    # Convert to pandas, index are group values, columns are metric values``
+    out = pd.DataFrame(out).T.astype({
+        'support': int
+    }).round(2).sort_values('support', ascending=False)
+    return out
+
+
+# TODO: add tests for this function
+def pretty_print_pandas(df: pd.DataFrame, index_rename: str = None) -> str:
+    """Pretty print pandas dataframe contents, using tabulate module.
+
+    Args:
+        df (pd.DataFrame): input pandas dataframe
+        index_rename (str, optional): how to rename index. Defaults to None
+        (index is not renamed.)
+    Returns:
+        str: pretty printed table
+    """
+
+    headers = df.head(0).reset_index()
+    if index_rename is not None:
+        headers = headers.rename({'index': index_rename}, axis=1)
+    headers = headers.columns.tolist()
+    to_print = tabulate.tabulate(df.itertuples(),
+                                 headers=headers,
+                                 tablefmt='psql')
+    return to_print
