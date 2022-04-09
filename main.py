d3335ce9c9e7e12f1bc58c37009ecceae474c508
marcospiau <marcospiau123@gmail.com>
2022-04-09 11:56:02 -0300

    Use typing.Literal to ensure type check for categorical variables for predictions via API

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 from collections import OrderedDict
+from typing import Literal
 
 import joblib
 import pandas as pd
@@ -34,19 +35,45 @@ app = FastAPI()
 class Input(BaseModel):
     """Data structure used for model input"""
     age: int
-    workclass: str
+    workclass: Literal['?', 'Federal-gov', 'Local-gov', 'Never-worked',
+                       'Private', 'Self-emp-inc', 'Self-emp-not-inc',
+                       'State-gov', 'Without-pay']
     fnlgt: int
-    education: str
+    education: Literal['10th', '11th', '12th', '1st-4th', '5th-6th', '7th-8th',
+                       '9th', 'Assoc-acdm', 'Assoc-voc', 'Bachelors',
+                       'Doctorate', 'HS-grad', 'Masters', 'Preschool',
+                       'Prof-school', 'Some-college']
     education_num: int = Field(alias='education-num')
-    marital_status: str = Field(alias='marital-status')
-    occupation: str
-    relationship: str
-    race: str
-    sex: str
+    marital_status: Literal['Divorced', 'Married-AF-spouse',
+                            'Married-civ-spouse', 'Married-spouse-absent',
+                            'Never-married', 'Separated',
+                            'Widowed'] = Field(alias='marital-status')
+    occupation: Literal['?', 'Adm-clerical', 'Armed-Forces', 'Craft-repair',
+                        'Exec-managerial', 'Farming-fishing',
+                        'Handlers-cleaners', 'Machine-op-inspct',
+                        'Other-service', 'Priv-house-serv', 'Prof-specialty',
+                        'Protective-serv', 'Sales', 'Tech-support',
+                        'Transport-moving']
+    relationship: Literal['Husband', 'Not-in-family', 'Other-relative',
+                          'Own-child', 'Unmarried', 'Wife']
+    race: Literal['Amer-Indian-Eskimo', 'Asian-Pac-Islander', 'Black', 'Other',
+                  'White']
+    sex: Literal['Female', 'Male']
     capital_gain: int = Field(alias='capital-gain')
     capital_loss: int = Field(alias='capital-loss')
     hours_per_week: int = Field(alias='hours-per-week')
-    native_country: str = Field(alias='native-country')
+    native_country: Literal['?', 'Cambodia', 'Canada', 'China', 'Columbia',
+                            'Cuba', 'Dominican-Republic', 'Ecuador',
+                            'El-Salvador', 'England', 'France', 'Germany',
+                            'Greece', 'Guatemala', 'Haiti',
+                            'Holand-Netherlands', 'Honduras', 'Hong',
+                            'Hungary', 'India', 'Iran', 'Ireland', 'Italy',
+                            'Jamaica', 'Japan', 'Laos', 'Mexico', 'Nicaragua',
+                            'Outlying-US(Guam-USVI-etc)', 'Peru',
+                            'Philippines', 'Poland', 'Portugal', 'Puerto-Rico',
+                            'Scotland', 'South', 'Taiwan', 'Thailand',
+                            'Trinadad&Tobago', 'United-States', 'Vietnam',
+                            'Yugoslavia'] = Field(alias='native-country')
 
     # salary: str
     # Make possible request using both attribute name and field alias
